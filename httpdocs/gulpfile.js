22ababdd509cb1d265841b298c01387361fabbda
moritz <kiehlmoritz@gmail.com>
2019-03-09 17:44:48 +0100

    [FEATURE] Addidtional Gulp/Npm setup, Add css-map, minor stylings

--- httpdocs/gulpfile.js
+++ httpdocs/gulpfile.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('./Resources/Private/SCSS/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('./Resources/Public/Css'));
 });
 
